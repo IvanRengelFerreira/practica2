fa909033778434b1288bff8655bce57c817f7a81
IvanRengelFerreira <ivan.rengelferreira@usp.ceu.es>
2024-03-03 18:52:29 +0100

    final

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,16 +1,14 @@
 package aplicacion;
-import java.util.Scanner;
 
 import mates.Matematicas;
 
 public class Principal{
 
-    //En principal impriminos el valor de PI siendo calculado con el método de Montecarlo con 1000000 de pasos, los pasos son el número de puntos generados y dara la cantidad de decimales que se quieren calcular
+    //En principal impriminos el valor de PI siendo calculado con el método de Montecarlo con 1000 de pasos, los pasos son el número de puntos generados y dara la cantidad de decimales que se quieren calcular
     //No use el args, ya que no es necesario y no me compilaba de esa manera
+    //Solo se puede con 1000 porque al sewr recursivo, se desborda la pila de llamadas
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese el número de decimales en funcion de potencias de 10 para calcular el número PI(Ejemplo: 1000000):");
-        long pasos = sc.nextLong();
-        System.out.println("El numero PI es " +Matematicas.generarNumeroPiRecursivo(0, 0, pasos));
+      
+        System.out.println("El numero PI es " +Matematicas.generarNumeroPiRecursivo(0, 0, 1000));
     }
 }
--- src/mates/Matematicas.java
+++ src/mates/Matematicas.java
@@ -12,6 +12,7 @@ public class Matematicas{
      * @return Una aproximación al número pi.
      */
     public static double generarNumeroPiRecursivo(int indice, long dentroCirculo, long pasos){
+    
         if(indice == pasos){
             return 4.0 * dentroCirculo / pasos;
         } else {
@@ -21,6 +22,7 @@ public class Matematicas{
             if(distancia < 1.0){
                 dentroCirculo++;
             }
+            //Llamada recursiva
             return generarNumeroPiRecursivo(indice+1, dentroCirculo, pasos);
         }
     }
